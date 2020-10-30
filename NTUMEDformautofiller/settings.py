0b890660a8928d2c036a62a47c656087e14ed7b9
Twb06 <31351207+Twb06@users.noreply.github.com>
2020-10-31 01:59:09 +0800

    alpha_v_1.0

--- NTUMEDformautofiller/settings.py
+++ NTUMEDformautofiller/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '0^^q&$ufvkzph=0pbrg_#ih6$5q-_l&c3-oy-^ymd$g+ug+)wq'
 import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
