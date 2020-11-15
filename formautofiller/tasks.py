9a6edd763d329960472e24b182cf0d589d7536e2
Twb06 <31351207+Twb06@users.noreply.github.com>
2020-11-16 01:49:58 +0800

    alpha_v_2.0

--- formautofiller/tasks.py
+++ formautofiller/tasks.py
@@ -11,11 +11,9 @@ import time
 def long_running_operation(self, user_autofill_url, user_clinical_teacher):
     print("long running operation started!@tasks.py")
 
-    #autofiller(self, user_autofill_url, user_clinical_teacher)
+    autofiller(self, user_autofill_url, user_clinical_teacher)
     progress_recorder = ProgressRecorder(self)
     for i in range(10):
         progress_recorder.set_progress(i, 9, description="Processing")
 
     print("long running operation completed!@tasks.py")
-
-    return
