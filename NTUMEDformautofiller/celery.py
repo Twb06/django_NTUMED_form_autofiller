4dde00414209545842a4c9d1bd58d9f2d6633e0b
Twb06 <31351207+Twb06@users.noreply.github.com>
2020-11-15 23:34:33 +0800

    alpha_v_2.0

--- NTUMEDformautofiller/celery.py
+++ NTUMEDformautofiller/celery.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
@@ -15,8 +15,8 @@ app = Celery('NTUMEDformautofiller')
 
 #app.config_from_object('django.conf:settings')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
+#app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
