3bbd375340a6ec30216c6ee27400d5fe76bd9bb3
Twb06 <31351207+Twb06@users.noreply.github.com>
2020-11-15 22:44:11 +0800

    alpha_v_2.0

--- formautofiller/tasks.py
+++ formautofiller/tasks.py
@@ -4,9 +4,17 @@ from celery import shared_task
 
 from .autofiller import autofiller
 
+from celery_progress.backend import ProgressRecorder
+import time
 
 @shared_task
 def long_running_operation(user_autofill_url, user_clinical_teacher):
     print("long running operation started!@tasks.py")
+
+    progress_recorder = ProgressRecorder(self)
+    for i in range(10):
+        progress_recorder.set_progress(i, 9, description="Processing")
+        time.sleep(0.5)
+
     autofiller(self,user_autofill_url, user_clinical_teacher)
     return
