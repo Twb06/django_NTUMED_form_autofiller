1de7e6384df737901b4a2ef9752f79de3a3fd1fd
Twb06 <31351207+Twb06@users.noreply.github.com>
2021-09-12 16:09:58 +0800

    alpha_v_4.1_no_module

--- NTUMEDformautofiller/settings.py
+++ NTUMEDformautofiller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = ['ntumed-form-autofiller.herokuapp.com','127.0.0.1']
 
